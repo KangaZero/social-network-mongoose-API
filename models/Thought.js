96c67e6bc50787e8d7e0415e4fc00b6a72e464cf
Samuel <samuelyongw@gmail.com>
2022-12-09 18:26:57 +1100

    Models added, need testing

---
+++ models/Thought.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  { 
+    timestamps: true 
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+thoughtSchema.virtual('reactionCount ').get(function () {
+  return this.reactions.length;
+});
+
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require('mongoose');
+const { validateEmail } = require('./utils/validateEmail');
+
+// Schema to create a course model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        [validateEmail, `Invalid email address`],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+      }
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      }
+    ],
+    dateCreated: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+
+      
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+
+const User = model('User', userSchema);
+
+module.exports = User;
